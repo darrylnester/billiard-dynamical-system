45b2480cbb89c0e4520547206ac64fa6d2a1e84d
darrylnester <37053130+darrylnester@users.noreply.github.com>
2023-09-29 14:37:49 -0400

    Update billiard.py

--- billiard.py
+++ billiard.py
@@ -31,9 +31,9 @@ def wall_collide(mass):
     return -mass['pos'] / mass['vel']
 
 
-def time_to_collision(mass1, mass2):
-    x1, x2 = mass1['pos'], mass2['pos']
-    v1, v2 = mass1['vel'], mass2['vel']
+def time_to_collision(m1, m2):
+    x1, x2 = m1['pos'], m2['pos']
+    v1, v2 = m1['vel'], m2['vel']
     delta_v = v1 - v2
     dist = x2 - x1
     if dist == 0 or delta_v * dist < 0 or abs(delta_v) < 1e-8:
@@ -41,33 +41,19 @@ def time_to_collision(mass1, mass2):
     return dist / delta_v
 
 
-def find_momentum(masses):
-    momentum = 0
-    for m in masses:
-        momentum += m['mass'] * m['vel']
-    return momentum
-
-
-norm_const = 1
-
-def norm(mass):
-    return mass['vel'] * norm_const
-
-
-def run_bounces(masses, report = False) -> dict:
-    global norm_const
+def run_bounces(masses) -> dict:
+    # masses is list of [mass, position, velocity]
     locations = [[m[1] for m in masses]]
     times = [0]
     masses = [new_mass(m[0], m[1], m[2]) for m in masses]
     N = len(masses)
-    # init_mom = find_momentum(masses)
-    norm_const = 1/math.sqrt(sum([m['vel']**2 * m['mass'] for m in masses]))
+    # this is the initial energy (doubled), which is constant
+    energy = sum([m['vel']**2 * m['mass'] for m in masses])
+    norm_coeffs = [math.sqrt(m['mass']/energy) for m in masses]
     collision_count = 0
     total_time = 0
-    vel_list = [ [m['vel']*norm_const for m in masses] ]
+    vel_list = [ [m['vel'] for m in masses] ]
     bounce_data = [ [m['pos'] for m in masses] ]
-    if report:
-        print(f"[{norm(masses[0]),norm(masses[1]),}", end='')
     while True:
         tmin = wall_collide(masses[0])
         indices = [(0, -1)]
@@ -113,26 +99,33 @@ def run_bounces(masses, report = False) -> dict:
                     # and should be fixed if we simply
                     # reset the positions to match.
                     m1['pos'] = m2['pos']
-        vel_list.append([m['vel']*norm_const for m in masses])
-        if report:
-            print(f",{norm(masses[0]),norm(masses[1])}", end='')
+        vel_list.append([m['vel'] for m in masses])
     # all collisions are done, just need to record the final details
-    if report: print(']')
     t = total_time * 0.1
     times.append(total_time + t)
     locations.append([m['pos'] + m['vel']*t for m in masses])
-    vel_list.append([m['vel']*norm_const for m in masses])
+    # TODO: add enough time so that the two most recent objects to
+    # collide are slightly separated ... (0.16 is a kludge)
+    bounce_data.append([0.16, [m['vel'] for m in masses]])
+    vel_list.append([m['vel']for m in masses])
     return {
         "masses": masses,
         "collisions": collision_count,
         "total_time": total_time,
         "vel": vel_list,
+        "coeffs": norm_coeffs,
         "locations": locations,
         "times": times,
         "bounces": bounce_data
     }
-    #     return locations, times
-    # return masses, collision_count, total_time
+
+
+def find_momentum(masses):
+    return sum([m['mass'] * m['vel'] for m in masses])
+    # momentum = 0
+    # for m in masses:
+    #     momentum += m['mass'] * m['vel']
+    # return momentum
 
 
 def get_final_momentum(masses):
@@ -166,7 +159,7 @@ def time3(x1, m3):
     return result["total_time"]
 
 
-def find_breaks(mass2=1, mass3=2):
+def find_breaks(m2=1, m3=2):
     # Starting with
     #    mass 1 at x1 (velocity 0)
     #    mass m2 at 1 (vel 0)
@@ -178,7 +171,7 @@ def find_breaks(mass2=1, mass3=2):
     pos_list = [0.00000001] \
         + [i/50+epsilon for i in range(1,50)] \
         + [0.99999999]
-    mom_list = [ (x1, mom3(x1, mass2, mass3)) for x1 in pos_list ]
+    mom_list = [ (x1, mom3(x1, m2, m3)) for x1 in pos_list ]
     breaks = []
     for i in range(len(mom_list)-1):
         x_left, mom_left = mom_list[i]
@@ -187,7 +180,7 @@ def find_breaks(mass2=1, mass3=2):
             # bisection search for the break ...
             while x_right - x_left > 1e-8:
                 x_mid = (x_right+x_left) / 2
-                mom_mid = mom3(x_mid, mass2, mass3)
+                mom_mid = mom3(x_mid, m2, m3)
                 if abs(mom_mid-mom_left) < 1e-6:
                     x_left = x_mid
                 else:
@@ -195,7 +188,7 @@ def find_breaks(mass2=1, mass3=2):
             breaks.append(x_left)
     return breaks
  
-
+# miscellaneous testing code
 # print(run_bounces([[1, 0.3, 0], [1, 1, 0], [9.1, 1.1, -1]])['bounces'])
 # for x1 in [0.3, 0.35, 0.7, 0.8]:
 #     print(x1, mom3(x1, 9.1))
